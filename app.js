4d2c45767f7d30aa5e7426f52cfca6c328f55a2f
惊鸿三世 <18810040762@163.com>
2015-10-28 23:58:18 +0800

    config the program

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * app.js - Simple express server 
+ * with middleware
+ */
+
+'use strict';
+var http = require('http'),
+	express = require('express'),
+	routes = require('./lib/routes'),
+	app = express(),
+	server = http.createServer(app);
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	//app.use(express.basicAuth('user', 'spa'));
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
+});
+app.configure('development', function() {
+	app.use(express.logger());
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
+});
+app.configure('production', function() {
+	app.use(express.errorHandler());
+})
+
+routes.configRoutes(app, server)
+
+server.listen(3000);
+console.log('Express server listening on port %d in %s mode', 
+	server.address().port, app.settings.env);
